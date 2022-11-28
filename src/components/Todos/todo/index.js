43d83afa12413469e884acac1b3b3eca97a4e384
sinan akyol <110792519+sinanakyol@users.noreply.github.com>
2022-11-28 23:59:03 +0300

    index.js changed

--- src/components/Todos/todo/index.js
+++ src/components/Todos/todo/index.js
@@ -1,32 +1,31 @@
 import React from "react";
 
-function Todo({ todos, setTodos }) {
-  const isCompleted = (e) =>
+function Todo({ todos, setTodos, todo }) {
+  const isCompleted = () =>
     setTodos(
       todos.map((item) => {
-        return item.id == e.target.id
+        return item.id === todo.id
           ? { ...item, completed: !item.completed }
           : item;
       })
     );
 
+  const deletedItem = () =>
+    setTodos(todos.filter((item) => item.id !== todo.id));
+
   return (
-    <ul className="todo-list">
-      {todos.map((todo) => (
-        <li key={todo.id} className={todo.completed ? "completed" : ""}>
-          <div className="view">
-            <input
-              className="toggle"
-              type="checkbox"
-              checked={todo.completed}
-              onChange={isCompleted}
-            />
-            <label>{todo.text}</label>
-            <button className="destroy"></button>
-          </div>
-        </li>
-      ))}
-    </ul>
+    <li key={todo.id} className={todo.completed ? "completed" : ""}>
+      <div className="view">
+        <input
+          className="toggle"
+          type="checkbox"
+          checked={todo.completed}
+          onChange={isCompleted}
+        />
+        <label>{todo.text}</label>
+        <button className="destroy" onClick={deletedItem}></button>
+      </div>
+    </li>
   );
 }
 
