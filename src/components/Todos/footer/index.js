4e8103ba75c92ae1c51c5e82344e3579e54a8721
sinan akyol <110792519+sinanakyol@users.noreply.github.com>
2022-11-29 02:21:20 +0300

    index.js changed

--- src/components/Todos/footer/index.js
+++ src/components/Todos/footer/index.js
@@ -1,28 +1,63 @@
 import React from "react";
 
-function Footer() {
+function Footer({ todos, setTodos, status, setStatus }) {
+  const unCompleted = todos.filter((todo) => todo.completed === false);
+  const completed = todos.filter((todo) => todo.completed === true);
+
+  const clearCompleted = () => {
+    setTodos(todos.filter((todo) => todo.completed === false));
+  };
+
+  const clickStyle = (e) => {
+    setStatus(e.target.id);
+  };
+
   return (
     <footer className="footer">
       <span className="todo-count">
-        <strong>2</strong>
+        <strong>{unCompleted.length} </strong>
         items left
       </span>
 
       <ul className="filters">
         <li>
-          <a href="#/" className="selected">
+          <a
+            href="#/"
+            className={status === "all" ? "selected" : ""}
+            id="all"
+            onClick={clickStyle}
+          >
             All
           </a>
         </li>
         <li>
-          <a href="#/">Active</a>
+          <a
+            href="#/"
+            className={status === "active" ? "selected" : ""}
+            id="active"
+            onClick={clickStyle}
+          >
+            Active
+          </a>
         </li>
         <li>
-          <a href="#/">Completed</a>
+          <a
+            href="#/"
+            className={status === "completed" ? "selected" : ""}
+            id="completed"
+            onClick={clickStyle}
+          >
+            Completed
+          </a>
         </li>
       </ul>
 
-      <button className="clear-completed">Clear completed</button>
+      <button
+        className={completed.length === 0 ? "hidden" : "clear-completed"}
+        onClick={clearCompleted}
+      >
+        Clear completed
+      </button>
     </footer>
   );
 }
